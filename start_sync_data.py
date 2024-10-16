824951c6e51993096e0926f9633e2b1b35b7ae81
jiajun.ma <1973908310@qq.com>
2024-10-16 10:10:13 +0800

    更改启动脚本执行顺序

--- start_sync_data.py
+++ start_sync_data.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     import zabbix_vcenter_data_sync_external
 
     vcenter_to_pgsql_sync.main()
-    pgsql_to_zabbix_sync.main()
     zabbix_vcenter_data_sync_external.main()
+    pgsql_to_zabbix_sync.main()
 
-    message_content = '\n'.join(vcenter_to_pgsql_sync.logger.handlers[0].logs + pgsql_to_zabbix_sync.logger.handlers[0].logs + zabbix_vcenter_data_sync_external.logger.handlers[0].logs)
+    message_content = '\n'.join(vcenter_to_pgsql_sync.logger.handlers[0].logs + zabbix_vcenter_data_sync_external.logger.handlers[0].logs + pgsql_to_zabbix_sync.logger.handlers[0].logs)
     send_message.dingtalk(message_content)
